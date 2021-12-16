bf750a1e98f52c05ce21a6ab5c71c087e2d083f6
Sam Bassong <sam.bassong@gmail.com>
2021-12-16 07:02:05 -0800

    landing modified a bit, about page styled and responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,13 +6,12 @@ const About = () => {
             <div className="about-container">
 
                 <div className="profile-info">
-                    <p>I am a full-Stack software engineer with a strong background in microbiology research and STEM education. I wrote my first line of code almost 2 years ago and haven't stopped since. What started as a hobby soon turned to active interest and I resolved to transition towards a software engineering career. Two months ago, I made the switch official by graduating from the software engineering track at General Assembly.</p>
-                    <p>For the past six months, I've been immersed in full-stack development. I'm having a blast continuously learning new technologies to build responsive, scalable, and user-focused applications. More recently, I was fortunate to land my first role in tech as as a <span className='teal'>Web Developer and Email Specialist Intern</span>. As a member of the Center Centre team, I am improving my skill while working towards a junior engineer role.</p>
+                    <p className="profile-p1">I am a full-Stack software engineer with a strong background in research and education. I wrote my first line of code 2 years ago and haven't stopped since. A year and a half later, Two months ago, I officially transitioned to a software engineering career by graduating from General Assembly's software engineering track.</p>
+                    <p className="profile-p2">As an Engineer, I'm leveraging my new and seasoned skills to find creative and elegant solutions. I'm enjoying learning modern technologies to build responsive, scalable and, user-friendly applications. I'm passionate about web development and cloud engineering and look forward to continuing my growth in this exciting industry.</p>
                 </div>
                 
+                
                 <div className='profile-container'>
-                    
-                    {/* <img  src="https://i.imgur.com/QGdnNtV.jpg" alt="sam avatar"/> */}
                     <div className="avatar-div"></div>
                     <div className="avatar-overlay"></div> 
                     <div className="avatar-frame"></div> 
