3ed900369cf42dcfdfffda6af17771113c6b07fd
Sam Bassong <sam.bassong@gmail.com>
2021-12-16 14:45:40 -0800

    project card and container style

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,14 +8,7 @@ const ProjectCard = ({title, description, image, link, technologies, github}) =>
 
                     <a href={link} target="_blank" rel="noreferrer">
                         <div className="project-details">
-                            <h1>{title}</h1>
-                            <h2 className="">
-                                <a href={link} key={image} rel="noreferrer" target="_blank" className="">Live site </a>
-                                | 
-                                <a href={github} key={image} rel="noreferrer" target="_blank" className=""> Github repo</a>
-                            </h2>
-                            <h2 className='technologies'>{technologies}</h2>
-                            <p className='description'>{description}</p>
+                            Click Me!
                         </div>            
                     </a>
                 </div>
