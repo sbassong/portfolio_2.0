a528dc0d084bcb0fe9885dda2a7488f63eecd8dd
Sam Bassong <sam.bassong@gmail.com>
2021-12-16 17:41:55 -0800

    top nav responsive

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,11 +1,38 @@
+import { useState, useEffect } from 'react'
+
+
 const TopNav = () => {
+
+    const [windowDimension, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+    })
+
+    const detectSize = () => {
+    detectHW({
+        winWidth: window.innerWidth,
+        winHeight: window.innerHeight,
+    })
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', detectSize)
+        return () => {
+        window.removeEventListener('resize', detectSize)
+        }
+    }, [windowDimension])
 
     return (
         <div className="top-nav">
             <div className="top-nav-wrap">
                 <a href="/">
                     <div className='top-left-nav'>
+                        {
+                        windowDimension.winWidth >= 450 ?
                         <img src="https://i.ibb.co/HPvd2c6/Screen-Shot-2021-12-09-at-10-29-23-AM-copy.png" alt="owl"/>
+                        :
+                        <img src="https://i.ibb.co/XSQRppT/Screen-Shot-2021-12-09-at-10-33-34-AM-copy-2.png" alt="owl"/>
+                        }
                     </div>
                 </a>
                 <nav className='top-right-nav'>
