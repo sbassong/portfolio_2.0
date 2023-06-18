2530e6f47cebd7c014cb9e43e1173647f3b44996
Sam Bassong <sam.bassong@gmail.com>
2023-06-18 12:49:23 -0700

    updated resume link

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -8,16 +8,14 @@ const TopNav = ({windowDimension}) => {
 		<div className="top-nav">
 			<div className="top-nav-wrap">
 				<NavLink className='top-left-nav' to='/'>
-					{/* <div > */}
-						<img src="https://i.ibb.co/HPvd2c6/Screen-Shot-2021-12-09-at-10-29-23-AM-copy.png" alt="owl"/>
-					{/* </div> */}
+					<img src="https://i.ibb.co/HPvd2c6/Screen-Shot-2021-12-09-at-10-29-23-AM-copy.png" alt="owl"/>
 				</NavLink>
 				<nav className='top-right-nav'>
-						<NavLink to='/' activeClassName="active">Home</NavLink>
-						<NavLink to='/projects' activeClassName="active">Projects</NavLink>
-						<NavLink to='/experience' activeClassName="active">Experience</NavLink>
-						<NavLink to='/skills' activeClassName="active">Skills</NavLink>
-						<a href="https://drive.google.com/file/d/1KGL9tYGPJXXDZDP1IHXL2VhRHknnCzWZ/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
+					<NavLink to='/' activeClassName="active">Home</NavLink>
+					<NavLink to='/projects' activeClassName="active">Projects</NavLink>
+					<NavLink to='/experience' activeClassName="active">Experience</NavLink>
+					<NavLink to='/skills' activeClassName="active">Skills</NavLink>
+					<a href="https://drive.google.com/file/d/1J122vv_P2wXVVKWlmMXXXcH8u0V9YSi8/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
 				</nav>
 			</div>
 		</div>
