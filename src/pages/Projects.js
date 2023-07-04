32edd2bbcda7f3869847ce0b63b52e02abdbc7b2
Sam Bassong <sam.bassong@gmail.com>
2023-07-03 17:42:41 -0700

    version 3.0 deployment

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Carousel from 'react-material-ui-carousel'
 import { professionalProjectsData } from "../data"
-import ProjectModal from "../components/ProjectModal"
 import ProjectCard from "../components/ProjectCard"
 
 const Projects = () => {
@@ -11,15 +9,6 @@ const Projects = () => {
             <div className='project-cards-container'>
                 { professionalProjectsData.map((project) => <ProjectCard key={project.id} project={project}/>) }
             </div>
-            {/* <Carousel
-                className='projects-carousel'
-                direction='10000'
-                navButtonsAlwaysVisible={true}
-                activeIndicatorIconButtonProps={{ style: { backgroundColor: '#52cdfe' }}}
-                navButtonsProps={{style: {height:  '2.5rem', width: '2.5rem', marginTop: '-2rem'}}}
-            >
-                { professionalProjectsData.map((project) => <ProjectModal key={project.id} project={project}/>) }
-            </Carousel> */}
         </div>
     )
     
