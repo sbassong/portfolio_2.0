e8a603e9436f6ab6938033ed4943f70eb8981ada
Sam Bassong <sam.bassong@gmail.com>
2023-07-04 19:44:01 -0700

    updated cards, ready to go, deployment 3

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,36 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import Fade from '@mui/material/Fade';
-import Zoom from '@mui/material/Zoom';
-import { styled } from '@mui/material/styles';
-// import { ThemeProvider } from '@emotion/react';
-import { muiOverrides } from '../styles/muiOverrides';
 import AgileUX from '../ProjectIcons/AgileUX';
 import Amphi from '../ProjectIcons/Amphi';
 import Gverse from '../ProjectIcons/Gverse';
 import Scienteer from '../ProjectIcons/Scienteer';
 
-const CustomWidthTooltip = styled(({ className, ...props }) => (
-	<Tooltip {...props} classes={{ popper: className }} />
-	))({
-		[`& .${tooltipClasses.tooltip}`]: {
-			maxWidth: 303,
-		},
-	});
 
 const ProjectCard = ({project}) => {
 	const [stack, setStack] = useState(null);
-	const [open, setOpen] = useState(false);
-
-  const handleClose = () => setOpen(false);
-  const handleOpen = () => setOpen(true);
 
 	useEffect(() => {
 		if (project.id === 1) setStack(<AgileUX id="project-card-techs" />);
 		else if (project.id === 2) setStack(<Gverse id="project-card-techs" />);
 		else if (project.id === 3) setStack(<Amphi id="project-card-techs" />);
 		else if (project.id === 4) setStack(<Scienteer id="project-card-techs" />);
-	}, [])
+	}, [project.id])
 
 	return (
 		<div className="project-card">
@@ -43,11 +26,11 @@ const ProjectCard = ({project}) => {
 				{stack}
 				{<div id="project-card-description">
 					<div className='inner'>
-						<span>{project.description}</span>
-						{/* { <div className="project-card-buttons">
+						{project.description}
+						{ <div className="project-card-buttons">
 							<a href={project.link} rel="noreferrer" target="_blank" className="">Demo</a>
 							<a href={project.github} rel="noreferrer" target="_blank" className="">Github</a>
-						</div>} */}
+						</div>}
 					</div>
 				</div>}
 
