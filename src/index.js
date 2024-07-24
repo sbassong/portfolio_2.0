3c9a4b663c3078186c7f28d157206b0f5de240c2
Sam Bassong <sam.bassong@gmail.com>
2024-07-23 19:27:20 -0700

    tried moving strictmode

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
