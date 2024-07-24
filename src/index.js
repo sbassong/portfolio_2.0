c1636c8133df72a21e2d38b3e0ba9a3cefa5eb36
Sam Bassong <72420711+sbassong@users.noreply.github.com>
2024-07-23 22:12:23 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
-const container = document.getElementById('root');
+let container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
