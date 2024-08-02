c65459361623491bcbbc0ef4f254491671f284a4
Sam Bassong <sam.bassong@gmail.com>
2024-08-01 17:56:46 -0700

    simplified landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,24 +4,16 @@ const Landing = () => {
   return (
     <div id='landing' className="page">
       <div className="about-wrapper">
-      <div className="about-container">
-        <div className='profile-container'>
-          <div className="avatar-div">
-          </div>
-        </div>
-        <div className="land-intro" >
           <div className="landing-title teal">
             Sam Bassong
           </div>
           <div className="landing-subtitle">
             Software Developer
           </div>
-          <div className="landing-subtitle teal">
-            I transform innovative ideas into powerful solutions.
-          </div>
+          <p class="teal left landing-blurb">Innovative ideas.</p>
+          <p class="teal center landing-blurb">Scalable designs.</p>
+          <p class="teal right landing-blurb">Powerful solutions.</p>
         </div>
-      </div>
-      </div>
     </div>
   );
 }
